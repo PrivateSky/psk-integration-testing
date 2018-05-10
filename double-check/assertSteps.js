1654e963ce044e545824b6f914a742121d20af77
Acer 2 <sava.dumitru.daniel@gmail.com>
2018-05-10 16:02:21 +0300

    integration tests

--- double-check/assertSteps.js
+++
@@ -1,9 +0,0 @@
-const assert = require("../../../modules/double-check").assert;
-var arr = [function a (next){
-    next();
-},
-    function b (next){
-        next();
-    }];
-
-assert.steps("Happy path test", arr);
---
+++ signsensus/consensusSymulationTest.js
@@ -0,0 +1,24 @@
+const consensus = require("../../modules/signsensus/lib/consensusManager");
+
+var cfg = require("../../../psk-integration-testing/signsensus/fakes/simulationConfig").config;
+var network = require("../../../psk-integration-testing/signsensus/fakes/comunicationFake");
+
+var cutil = require("../../modules/signsensus/lib/consUtil");
+
+network.init();
+
+
+var numberTransactions = cfg.MAX_TRANSACTIONS;
+
+
+
+while(numberTransactions > 0){
+    setTimeout(function () {
+        network.generateRandomTransaction();
+        //console.log("New transaction!");
+    }, cutil.getRandomInt(cfg.MAX_TRANSACTION_TIME));
+    numberTransactions--;
+}
+
+console.log(cfg.MAX_TRANSACTIONS,numberTransactions);
+
