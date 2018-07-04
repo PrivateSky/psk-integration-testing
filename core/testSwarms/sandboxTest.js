088a618c96e1b276671c206b1364dab0c7b958ca
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2018-07-04 15:33:19 +0300

    updating sandboxTest

--- core/testSwarms/sandboxTest.js
+++ core/testSwarms/sandboxTest.js
@@ -5,23 +5,36 @@ var f = $$.swarm.create("testSandBoxExecution", {
     public:{
         result:"int"
     },
-    init:function(){
+    protected:{
+		cb:"function"
+    },
+    init:function(cb){
         this.result = 0;
-        this.swarm("space1\\agent\\agent_x", "test1");
-
+        this.cb = cb;
+        this.swarm("space1\\agent\\agent_x", "test1").onReturn(this.lastReturn);
+        this.cb = cb;
+    },
+    lastReturn:function(){
+		if(this.cb){
+			this.cb();
+		}
+    },
+    intermediaryReturn:function(){
+        this.return();
     },
     test1:function(){
         assert.equal(this.result,0,"Unexpected result");
         this.result += 1;
-        this.swarm("space2\\agent\\agent_007", "test2");
+        this.swarm("space2\\agent\\agent_007", "test2").onReturn(this.intermediaryReturn);
     },
     test2:function(){
         assert.equal(this.result,1,"Unexpected result");
         this.result += 2;
-        this.swarm("crl\\agent\\jhon_smith", "onResult");
+        this.swarm("crl\\agent\\jhon_smith", "onResult").onReturn(this.intermediaryReturn);
     },
     onResult:function(err, text){
         assert.equal(err,undefined,"Error");
         assert.equal(this.result,3,"Unexpected result");
+        this.return();
     }
 });
