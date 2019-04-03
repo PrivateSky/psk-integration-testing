6a18b0a62533ba50b6c61807c7b9bdff2396e284
mastaleru.rafael@gmail.com <mastaleru.rafael@gmail.com>
2019-04-03 17:31:56 +0300

    interceptor test update

--- core/interceptorChainTest2.js
+++ core/interceptorChainTest2.js
@@ -1,68 +1,88 @@
 require("../../../builds/devel/pskruntime");
-// require("callflow");
-// require("launcher");
-var assert = require("double-check").assert;
+const tir = require('./../test-util/tir.js');
+const assert = require('double-check').assert;
 
-//inspired by `swarmBasicTest.js`
-var f = $$.swarms.describe("MySwarm", {
-    type: "flow",       // flow, key, contract
-    public: {
-        a1: "int",
-        a2: "int",
-        result: "int",
-        interceptorCollector: String,
+const domain = 'local';
+const agents = [
+    'agent_007'
+];
+
+const swarm = {
+    MySwarm: {
+        public: {
+            a1: "int",
+            a2: "int",
+            result: "int",
+            interceptorCollector: "string",
+        },
+        begin: function (a1, a2) {
+            this.a1 = a1;
+            this.a2 = a2;
+            // this.start('space1\\agent\\agent_007', "doStep", 3).onReturn(this.otherExecution);
+            //this.swarm('agent_007', "doStep", 3);
+            this.doStep(3);
+        },
+        doStep: function (a) {
+            this.result = this.a1 + this.a2 + a;
+            //this.swarm("exampleAgent", "otherExecution");
+            this.otherExecution(null);
+        },
+        otherExecution: function (err, res, wholeSwarm) {
+            var assert = require("double-check").assert;
+            assert.equal(err, null, "Error");
+            //assert.equal(this.result, undefined, "this.result should be undefined");
+            //assert.equal(res, 6, "Invalid value of res");
+            //this.update(wholeSwarm);
+            //assert.equal(this.result, 6, "this.result should be 6 after update");
+            this.return();
+        }
     },
-    resetInterceptorCollector: function () {
-        this.interceptorCollector = '===';
-    },
-    begin: function (a1, a2, callback) {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.callback = callback;
-        this.startSwarm('global.MySwarm', "doStep", 3).onReturn(this.otherExecution);    //Nobody listening for <swarm_for_execution>!
-    },
-    doStep: function (a) {
-        this.result = this.a1 + this.a2 + a;
-        this.return(null, this.result);
-    },
-    otherExecution: function (err, res, wholeSwarm) {
-        this.update(wholeSwarm);
-        this.callback();
+
+    InterceptorSwarm: {
+        setup: function () {
+            $$.interceptor.register('MySwarm', 'begin', 'before', function () {
+                this.interceptorCollector += ' begin_before';
+                console.log(this.interceptorCollector);
+            });
+            $$.interceptor.register('MySwarm', 'begin', 'after', function () {
+                this.interceptorCollector += ' begin_after';
+                console.log(this.interceptorCollector);
+            });
+            $$.interceptor.register('MySwarm', 'doStep', 'before', function () {
+                this.interceptorCollector += ' doStep_before';
+                console.log(this.interceptorCollector);
+            });
+            $$.interceptor.register('MySwarm', 'doStep', 'after', function () {
+                this.interceptorCollector += ' doStep_after';
+                console.log(this.interceptorCollector);
+            });
+            $$.interceptor.register('MySwarm', 'otherExecution', 'before', function () {
+                this.interceptorCollector += ' otherExecution_before';
+                console.log(this.interceptorCollector);
+            });
+            $$.interceptor.register('MySwarm', 'otherExecution', 'after', function () {
+                this.interceptorCollector += ' otherExecution_after';
+                console.log(this.interceptorCollector);
+            });
+
+            this.return();
+        }
     }
-})();
+};
 
+assert.callback('test interceptor call order', (finished) => {
+    tir.addDomain(domain, agents, swarm).launch(5000, () => {
+        tir.interact('local', 'agent_007').startSwarm("InterceptorSwarm", "setup")
+        .onReturn( () => {
+                console.log("interceptor set for agent_007");
+                tir.interact('local', 'agent_007')
+                .startSwarm("MySwarm", "begin", 1, 2)
+                .onReturn(result => {
+                    // assert.equal("Echo Hello", result);
+                    finished();
+                    tir.tearDown(0);
+                });
+        });
 
-assert.true(false,"IT DOES NOT WORK: 'Nobody listening for <swarm_for_execution>!'");
-throw "Grrrrrrrrrrrrrrrrrrrrrrr...";
-
-assert.callback("test_interceptor_call_order_when_swarm_methods_invokes_anothers", function (callback) {
-    f.resetInterceptorCollector();
-
-    $$.interceptor.register('MySwarm', 'begin', 'before', function () {
-        this.interceptorCollector += ' begin_before';
-        console.log(this.interceptorCollector);
     });
-    $$.interceptor.register('MySwarm', 'begin', 'after', function () {
-        this.interceptorCollector += ' begin_after';
-        console.log(this.interceptorCollector);
-    });
-    $$.interceptor.register('MySwarm', 'doStep', 'before', function () {
-        this.interceptorCollector += ' doStep_before';
-        console.log(this.interceptorCollector);
-    });
-    $$.interceptor.register('MySwarm', 'doStep', 'after', function () {
-        this.interceptorCollector += ' doStep_after';
-        console.log(this.interceptorCollector);
-    });
-    $$.interceptor.register('MySwarm', 'otherExecution', 'before', function () {
-        this.interceptorCollector += ' otherExecution_before';
-        console.log(this.interceptorCollector);
-    });
-    $$.interceptor.register('MySwarm', 'otherExecution', 'after', function () {
-        this.interceptorCollector += ' otherExecution_after';
-        console.log(this.interceptorCollector);
-    });
-
-    f.begin(1, 2, callback);
-    console.log(f.interceptorCollector);
-})
+}, 3500);
