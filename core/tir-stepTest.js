fd20d63da93f1d6c3809ce3347b6cad72ca6cb29
HS\vlad.constantin <vlad.radu@heavensolutions..com>
2019-06-04 15:44:49 +0300

    ported stepTest to TIR

---
+++ core/tir-stepTest.js
@@ -0,0 +1,43 @@
+const tir = require('../test-util/tir.js');
+require("callflow");
+const assert = require("double-check").assert;
+
+const domain = 'local';
+const agents = ['agent'];
+
+const swarm = {
+    stepExample :{
+        private:{
+            a1:"int",
+            a2:"int"
+        },
+        public:{
+            result:"int"
+        },
+        begin:function(a1,a2){
+            this.a1 = a1[0][0];
+            this.a2 = a1[0][1];
+            console.log(a1);
+            this.doStep(3);
+            this.return(0);
+        },
+        doStep:function(a){
+            this.result = this.a1 + this.a2 + a;
+            this.doResult();
+        },
+        doResult:function(){
+            const assert = require('double-check').assert;
+            assert.equal(this.result,6,"Results don't match");
+        }
+    }
+};
+
+assert.callback("Step test",function (finish) {
+    tir.addDomain(domain, agents, swarm).launch(5000, () => {
+        tir.interact (domain, agents[0]).startSwarm("stepExample", "begin",1, 2).onReturn(() =>{
+            finish();
+            tir.tearDown(0);
+        });
+    })  
+}, 3000)
+
