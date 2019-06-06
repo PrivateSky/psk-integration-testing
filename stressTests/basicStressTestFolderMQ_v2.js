1a16d3c5d38aee2721ad4d33554275979d88466c
Dev <vlad.lupu@heavensolutions.com>
2019-06-06 16:38:34 +0300

    cross domains/same domain chain  tests

--- stressTests/basicStressTestFolderMQ_v2.js
+++ stressTests/basicStressTestFolderMQ_v2.js
@@ -4,8 +4,13 @@ const domain = 'superdomain';
 const noAgents = 10;
 var agents = [];
 
-for(var i=0; i<noAgents; i++){
-    agents.push("superAgent_"+Math.random().toString(36).substr(2, 9));
+for (var i = 0; i < noAgents; i++) {
+  agents.push(
+    'superAgent_' +
+      Math.random()
+        .toString(36)
+        .substr(2, 9)
+  );
 }
 
 const swarm = {
@@ -15,49 +20,53 @@ const swarm = {
     }
   },
   second: {
-    phase: function(){
+    phase: function() {
       this.return(1);
     }
   }
 };
 
 var index = 0;
-function getAgent(){
-    if(index >= noAgents){
-        index = 0;
-    }
-    const agent = agents[index];
-    index++;
-    return agent;
+function getAgent() {
+  if (index >= noAgents) {
+    index = 0;
+  }
+  const agent = agents[index];
+  index++;
+  return agent;
 }
 
-assert.callback('Basic stress test for FolderMQ vers. 2', (finished) => {
+assert.callback(
+  'Basic stress test for FolderMQ vers. 2',
+  finished => {
     var runs = 1000;
-    var targetedResult = runs*1000+runs*runs*1;
+    var targetedResult = runs * 1000 + runs * runs * 1;
     var result = 0;
 
     tir.addDomain(domain, agents, swarm).launch(550000, () => {
-        var callbacks = 0;
-        for(let i=0; i<runs; i++){
-            var ti = tir.interact(domain, getAgent());
+      var callbacks = 0;
+      for (let i = 0; i < runs; i++) {
+        var ti = tir.interact(domain, getAgent());
 
-            ti.startSwarm("first", "phase").onReturn(firstSwarmRes => {
-            result += firstSwarmRes;
+        ti.startSwarm('first', 'phase').onReturn(firstSwarmRes => {
+          result += firstSwarmRes;
 
-            for(let j=0; j<runs; j++){
-                ti.startSwarm("second", "phase").onReturn(secondSwarmRes => {
-                    callbacks++;
-                    result += secondSwarmRes;
-                    console.log(`Received <${callbacks}> swarm results from a total of <${runs*runs}>`);
-                    if(callbacks == runs*runs){
-                        console.log("Result", result, "Expected result", targetedResult);
-                        assert.equal(targetedResult, result, "Expected to get "+targetedResult);
-                        finished();
-                        tir.tearDown(0);
-                    }
-              });
-            }
-          });
-        }
+          for (let j = 0; j < runs; j++) {
+            ti.startSwarm('second', 'phase').onReturn(secondSwarmRes => {
+              callbacks++;
+              result += secondSwarmRes;
+              console.log(`Received <${callbacks}> swarm results from a total of <${runs * runs}>`);
+              if (callbacks == runs * runs) {
+                console.log('Result', result, 'Expected result', targetedResult);
+                assert.equal(targetedResult, result, 'Expected to get ' + targetedResult);
+                finished();
+                tir.tearDown(0);
+              }
+            });
+          }
+        });
+      }
     });
-}, 500000);
+  },
+  500000
+);
