4324307e999f0d3b6bceb31a5c2d203bd868d3e4
Maco090 <dragosmacovei99@yahoo.com>
2019-06-13 12:16:45 +0300

    psky-schema integration tests

---
+++ psky-schema-negative/swarmStructure.js
@@ -0,0 +1,37 @@
+const tir = require('./../test-util/tir.js');
+const assert = require('double-check').assert;
+const domain = 'local';
+const agents = ['hq', 'mi6'];
+
+const swarms = {
+    intel: {
+        public: {
+            a1: 'integer',
+            a2: 'integer',
+            result: 'integer'
+        },
+        test: {
+            a3: 'integer',
+            a4: 'integer'
+        },
+        begin: function (a1, a2) {
+            this.a1 = a1;
+            this.a2 = a2;
+            this.result = this.a1 = this.a2;
+
+            console.info('--------- WE ARE THIS:', this);
+            this.return(this.result);
+        }
+    }
+};
+
+
+assert.callback('Swarm Structure Phase type checking', (finished) => {
+    tir.addDomain(domain, agents, swarms).launch(3000, () => {
+        tir.interact('local', 'hq').startSwarm('intel', 'begin', 1, 2).onReturn((result) => {
+            console.log("Test should not pass. Phase must be a function");
+            // finished();
+        });
+    });
+
+}, 2000);
