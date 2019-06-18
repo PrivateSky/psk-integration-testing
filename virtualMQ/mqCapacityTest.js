f87a8704292ace23f1878b3c4c5ac6f55942d340
adrianganga@yahoo.com <adiganga2002@yahoo.com>
2019-06-18 12:45:05 +0300

    Test for virtualMQ capacity operations per second

--- virtualMQ/mqCapacityTest.js
+++ virtualMQ/mqCapacityTest.js
@@ -11,7 +11,7 @@ require("../../../builds/devel/psknode");
 
 const utils = require("../../psk-unit-testing/Utils/fileUtils");
 const assert = require("double-check").assert;
-const VirtualMQ        = require('../../../modules/virtualmq');
+const VirtualMQ = require('../../../modules/virtualmq');
 const CHANNEL_NAME = Buffer.from('testChannel').toString('base64');
 const fs = require("fs");
 const http = require("http");
@@ -26,8 +26,12 @@ try {
 
 let port = 8092;
 const args = process.argv.slice(2);
-var minQueueCapacity = args[0] || 4000;
-var postCallsTimeout = args[1] || 10000;
+var maxQueueCapacity = args[0] || 0;
+var waitStrutureTimeout = 5000;
+var nrOfSeconds = 5;
+var postCallsTimeout = args[1] || waitStrutureTimeout+nrOfSeconds*1000;
+
+var expectedMin = 450;
 var postFlag = true;
 
 //var server = new Server(sslConfig).listen(port);
@@ -36,7 +40,7 @@ function createServer(callback) {
         if (err) {
             console.log("Failed to create VirtualMQ server on port ", port);
             console.log("Trying again...");
-            if (port > 0 && port < 50000) {
+            if (port > 1024 && port < 50000) {
                 port++;
                 createServer(callback);
             } else {
@@ -49,7 +53,7 @@ function createServer(callback) {
     });
 }
 
-function createSwarmMessage(msg) {
+function createSwarmMessage(msg, finish) {
     return JSON.stringify({
         meta: {
             swarmId: msg
@@ -57,7 +61,7 @@ function createSwarmMessage(msg) {
     });
 }
 
-var counter = 0;
+
 const options = {
     host: '127.0.0.1',
     port: port,
@@ -65,54 +69,109 @@ const options = {
     method: 'POST'
 };
 
+var maxCounter = 0;
 var req;
+var myInterval;
+var remaining;
+var start, end;
+
+function getDefaultMQCapacity(msg, finish) {
+    req = http.request(options, (res) => {
+        let rawData = '';
+        res.on('data', (chunk) => {
+            rawData += chunk;
+        });
+        res.on('end', () => {
+            maxQueueCapacity = res.headers['x-ratelimit-limit'];
+            var postRequestsArr = [];
+            let messages = [];
+
+            //prepare bulk of requests
+            for (let i = 0; i < maxQueueCapacity * 2; i++) {
+                messages.push(createSwarmMessage(i));
+            }
+            //-----------
+
+            //try to flood the queue per second
+            //wait 5 sec to finish creating folder structure
+            var counter = 0;
+            setTimeout(()=>{
+
+                start = (new Date()).getTime();
+                setInterval(()=>{
+                    for (let j = 0; j < 50; j++) {
+                        try {
+                            counter++;
+                            postMessage(messages[j]);
+
+
+                        } catch (e) {
+                            console.log('-------------', counter, ' -------------- ', j, ' eeeeee', e);
+                            break;
+                        }
+
+                    }
+                },1);
+            }, waitStrutureTimeout);
+
+
+                //postMessage(createSwarmMessage('msg_' + counter), finish)
+
+        });
+    });
+
+    req.on('error', function (e) {
+        return;
+    });
+
+    req.write(msg);
+    req.end();
+
+}
 
 // Make a post with a message
-function postMessage(message, finish) {
+function postMessage(message) {
 
+    req = http.request(options, (res) => {
+        const statusCode = res;
+        end = (new Date()).getTime();
+        maxCounter++;
+        remaining = Number(res.headers['x-ratelimit-remaining']);
+        if (res.statusCode != 201) {
+            res.resume();
+            return 'the ID IS ' + JSON.parse(message).meta['swarmId'];
+        }
 
-    try {
-        req = http.request(options, (res) => {
-            const statusCode = res;
-            let error;
-            if (statusCode >= 400) {
-                error = new Error('Request Failed.\n' +
-                    `Status Code: ${statusCode}`);
-            }
-
-            if (error) {
-                console.log(error);
-                res.destroy();
-                return;
-            }
-
-            let rawData = '';
-            res.on('data', (chunk) => {
-                rawData += chunk;
-            });
-            res.on('end', ()=>{})
+        let rawData = '';
+        res.on('data', (chunk) => {
+            rawData += chunk;
         });
+        res.on('end', () => {
+        })
+    });
 
-        req.on('error',function(e){
-            return;
-        });
+    req.on('error', function (e) {
+        e.swarmId = JSON.parse(message).meta['swarmId'];
+        e.maxCounter = maxCounter;
+        e.remaining = remaining;
+        e.starTime = start;
+        e.endTime = end;
+        e.totalTime = end - start;
 
-        counter++;
+        throw e;
 
-        req.write(message);
-        req.end();
-        postMessage(createSwarmMessage('msg_' + counter), finish);
 
-    } catch (e) {
-        return;
-    }
+    });
+    req.write(message);
+    req.end();
 
 }
 
 
 function endAndCleanTest(finish) {
-    console.log('Number of post messages sent to virtualMQ is: ', counter, ' expected minimum ', minQueueCapacity);
-    assert.true(counter > minQueueCapacity, 'virtualMQ failed after ' + counter + ' post calls');
+    console.log('Number of post messages sent to virtualMQ per second is: ', maxCounter/nrOfSeconds, ' total time ', end - start);
+
+    assert.true(maxCounter > expectedMin*nrOfSeconds, 'virtualMQ failed after ' + maxCounter + ' post calls');
     utils.deleteFolder(folder);
     finish();
     process.exit(0);
@@ -120,13 +179,26 @@ function endAndCleanTest(finish) {
 }
 
 function test(finish) {
+    getDefaultMQCapacity(createSwarmMessage('msg'), finish);
+    process.on
+    (
+        'uncaughtException',
+        function (err) {
+            console.log('MQ Fail ', err);
+            utils.deleteFolder(folder);
+            process.exit(0);
+        }
+    );
+
     setTimeout(() => {
         utils.deleteFolder(folder);
         endAndCleanTest(finish);
     }, postCallsTimeout);
-    postMessage(createSwarmMessage('msg_' + counter), finish);
+
+
 }
 
 createServer((server) => {
+
     assert.callback("VirtualMQ stress test", test, postCallsTimeout + 1000);
 });
