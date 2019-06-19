2f109d7fe79cb96c1a07834671b85d680cf95224
adrianganga@yahoo.com <adiganga2002@yahoo.com>
2019-06-19 18:59:42 +0300

    Add simultaneous read test from virtualMQ 2 scenarios and create an utils lib for virtualMQ

--- virtualMQ/mqCapacityTest.js
+++ virtualMQ/mqCapacityTest.js
@@ -9,7 +9,7 @@ require("../../../builds/devel/pskruntime");
 require("../../../builds/devel/virtualMQ");
 require("../../../builds/devel/psknode");
 
-const utils = require("../../psk-unit-testing/Utils/fileUtils");
+const utils = require("../../psk-unit-testing/Utils/virtualMQUtils");
 const assert = require("double-check").assert;
 const VirtualMQ = require('../../../modules/virtualmq');
 const CHANNEL_NAME = Buffer.from('testChannel').toString('base64');
