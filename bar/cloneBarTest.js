0658a00129508d960fe9b7666afe0f8db124d924
skutner <sava.dumitru.daniel@gmail.com>
2019-10-03 13:53:27 +0300

    refactoring bar clone test

--- bar/cloneBarTest.js
+++ bar/cloneBarTest.js
@@ -1,69 +1,98 @@
 require("../../../psknode/bundles/pskruntime");
 require("../../../psknode/bundles/psknode");
+require("callflow");
 const bar = require('bar');
 const double_check = require("../../../modules/double-check");
 const assert = double_check.assert;
 const Archive = bar.Archive;
 const ArchiveConfigurator = bar.ArchiveConfigurator;
+const fs = require("fs");
 const crypto = require("crypto");
 const path = require("path");
-const fs = require("fs");
 
-double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
+let folderPath;
+let filePath;
+let savePath;
+let cloneStoragePath;
 
-    const folderPath = path.join(testFolder, "fld");
-    let savePath = path.join(testFolder, "dot");
-    let additionalStoragePath = path.join(testFolder, "aux");
 
-    const folders = ["fld/fld2"].map(folder => path.join(testFolder, folder));
-    const files = ["fld/a.txt", "fld/fld2/b.txt"].map(file => path.join(testFolder, file));
+let folders;
+let files;
+const text = ["asta e un text", "asta e un alt text", "ana are mere"];
 
-    const text = ["asta e un text", "asta e un alt text"];
+$$.flows.describe("BarClone", {
+    start: function (callback) {
+        this.callback = callback;
 
-    const fileBrickStorage = bar.createFileBrickStorage(additionalStoragePath);
-
-    const archiveConfigurator = new ArchiveConfigurator();
-    archiveConfigurator.setStorageProvider("FileBrickStorage", savePath);
-    archiveConfigurator.setFsAdapter("FsAdapter");
-    archiveConfigurator.setBufferSize(2);
-    archiveConfigurator.setMapEncryptionKey(crypto.randomBytes(32));
-
-    const archive = new Archive(archiveConfigurator);
-
-    assert.callback("Clone Bar Test", (callback) => {
         double_check.ensureFilesExist(folders, files, text, (err) => {
             assert.true(err === null || typeof err === "undefined", "Failed to create folder hierarchy.");
 
-            double_check.computeFoldersHashes([folderPath], (err, initialHashes) => {
+            double_check.computeFoldersHashes(folderPath, (err, initialHashes) => {
                 assert.true(err === null || typeof err === "undefined", "Failed to compute folder hashes.");
 
-                archive.addFolder(folderPath, (err, mapDigest) => {
-                    assert.true(err === null || typeof err === "undefined", "Failed to add folder.");
-
-                    double_check.deleteFoldersSync(folderPath);
-                    assert.true(err === null || typeof err === "undefined", "Failed to delete folders.");
-
-                    archive.clone(fileBrickStorage, true, (err) => {
-                        assert.true(err === null || typeof err === "undefined", "Failed to clone archive.");
-
-                        archive.extractFolder(additionalStoragePath, (err) => {
-                            assert.true(err === null || typeof err === "undefined", "Failed to extract folder.");
-
-                            double_check.computeFoldersHashes([folderPath], (err, extractionHashes) => {
-                                assert.true(err === null || typeof err === "undefined", "Failed to compute folder hashes.");
-                                assert.true(assert.hashesAreEqual(initialHashes, extractionHashes), "Folder hashes do not coincide.");
-
-                                double_check.deleteFoldersSync(folderPath);
-                                fs.unlinkSync(savePath);
-                                fs.unlinkSync(additionalStoragePath);
-                                assert.true(err === null || typeof err === "undefined", "Failed to delete folders.");
-                                callback();
-                            });
-                        });
-                    });
-                });
+                this.initialHashes = initialHashes;
+                this.createArchive();
             });
         });
-    }, 2000);
+
+    },
+
+    createArchive: function () {
+        this.archiveConfigurator = new ArchiveConfigurator();
+        this.archiveConfigurator.setStorageProvider("FileBrickStorage", savePath);
+        this.archiveConfigurator.setFsAdapter("FsAdapter");
+        this.archiveConfigurator.setBufferSize(2);
+        this.archiveConfigurator.setMapEncryptionKey(crypto.randomBytes(32));
+        this.archive = new Archive(this.archiveConfigurator);
+
+        this.folderBrickStorage = bar.createFolderBrickStorage(cloneStoragePath);
+        this.addFolder();
+    },
+
+    addFolder: function () {
+        this.archive.addFolder(folderPath, (err, mapDigest) => {
+            assert.true(err === null || typeof err === "undefined", "Failed to add folder.");
+
+            double_check.deleteFoldersSync(folderPath);
+            this.cloneBar();
+        });
+    },
+
+    cloneBar: function () {
+        this.archive.clone(this.folderBrickStorage, true, (err, mapDigest) => {
+            assert.true(err === null || typeof err === "undefined", `Failed to delete file ${filePath}`);
+            assert.true(mapDigest !== null && typeof mapDigest !== "undefined", "Map digest is null or undefined");
+
+            this.extractFolder();
+        });
+    },
+
+    extractFolder: function () {
+        this.archive.extractFolder(cloneStoragePath, (err) => {
+            assert.true(err === null || typeof err === "undefined", `Failed to extract folder from file ${savePath}`);
+
+            double_check.computeFoldersHashes(folderPath, (err, newHashes) => {
+                assert.true(err === null || typeof err === "undefined", "Failed to compute folder hashes.");
+                assert.hashesAreEqual(this.initialHashes, newHashes, "The extracted files are not te same as the initial ones");
+
+                double_check.deleteFoldersSync([folderPath, cloneStoragePath]);
+                fs.unlinkSync(savePath);
+                this.callback();
+            });
+        });
+    }
 });
 
+double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
+
+    folderPath = path.join(testFolder, "fld");
+    savePath = path.join(testFolder, "dot");
+    cloneStoragePath = path.join(testFolder, "aux");
+
+    folders = [folderPath, cloneStoragePath];
+    files = ["fld/a.txt", "fld/b.txt", "fld/c.txt"].map(file => path.join(testFolder, file));
+
+    assert.callback("Bar clone test", (callback) => {
+        $$.flows.start("BarClone", "start", callback);
+    }, 3000);
+});
